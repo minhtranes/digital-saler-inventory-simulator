315fcc10a5bdcfc8f811e795184ccdcd208aedad
Minh Tran <wisekingtran@gmail.com>
2022-01-25 09:22:06 +0700

    Update build structure

---
+++ src/main/resources/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+JAR_FILE=digital-saler-inventory-simulator.jar
+CURRENT_DIR=$PWD
+
+java -Dcom.sun.management.jmxremote=true \
+    -Dcom.sun.management.jmxremote.port=5000 \
+    -Dcom.sun.management.jmxremote.authenticate=false \
+    -Dcom.sun.management.jmxremote.ssl=false \
+    -Dcom.sun.management.jmxremote.local.only=false \
+    -Dlogging.config=$CURRENT_DIR/config/logback.xml \
+    -Djavax.net.ssl.trustStore=$CURRENT_DIR/config/truststore.jks \
+    -Djavax.net.ssl.trustStorePassword=changeit \
+    -jar $JAR_FILE
